ea653074f16984499bbe480e1f0c272dec03bf6b
Saqib Aziz <saqib.aziz41@gmail.com>
2022-06-21 07:12:16 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const pathjs = require('path');
+const engine = require('ejs-mate');
+
+// middlewares
+app.engine('ejs', engine);
+app.set('view engine', 'ejs')
+app.set('views', pathjs.join(__dirname, 'views'));
+app.use(express.static(pathjs.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routing
+
+
+// start server
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+    console.log('Server is running at port #', port);
+})

a22a85f67f4dbed05f8a2f7911a685ed4eb15258
Saqib Aziz <saqib.aziz41@gmail.com>
2022-06-30 04:56:26 -0700

    landing page designed

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/homeController')
 
-
+router.get('/', (req, res) => {})
 router.route('/login')
 .get(homeController.renderLogin)
 .post(homeController.loginHandler);
